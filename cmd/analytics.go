e48faf9865391626b8fa0e709653c4289ebb31ec
luqmansen <luqmansen@gmail.com>
2019-11-21 22:51:37 +0700

    parse date succes

--- cmd/analytics.go
+++ cmd/analytics.go
@@ -1,14 +1,19 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/spf13/cobra"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
+const (
+	layout = "02/Jan/2006:15:04:05"
+)
 
 func checkEr(e error)  {
 	if e != nil{
@@ -16,22 +21,42 @@ func checkEr(e error)  {
 	}
 }
 
-func checkFile(dir string, filename string) (*os.File, error){
-	dat, err := ioutil.ReadFile(dir + "/" + filename)
+func openFile(dir string, filename string) (*os.File){
+	file, err := os.Open(dir + "/" + filename)
+	fmt.Println(dir +"/"+ filename)
+	checkEr(err)
+	return file
+}
 
-	checkEr(err)
-	fmt.Println(string(dat))
-	fmt.Println(dir + "/"  +filename)
-	f, err := os.Open(dir + "/"  +filename)
-	checkEr(err)
-	return f, nil
+func readFile(f *os.File) *bufio.Scanner{
+	scanner := bufio.NewScanner(f)
+	defer f.Close()
+	for scanner.Scan(){
+		line := strings.Fields(scanner.Text())
+		parseLine(line)
+	}
+	return scanner
+}
+
+func parseLine(s []string) bool{
+	val := strings.Replace(s[3], "[", "", -1)
+	//fmt.Println(val)
+	t, err := time.Parse(layout, val)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(t.Unix())
+
+	return true
 }
 
 
 func analytics(cmd * cobra.Command, args []string){
-	dir, _ := cmd.Flags().GetString("directory")
-	fmt.Println(dir)
-	checkFile(dir, "access.log.1")
+	//dir, _ := cmd.Flags().GetString("directory")
+	//fmt.Println(dir)
+	f := openFile(".", "access.log.1")
+	readFile(f)
+
 
 
 }
