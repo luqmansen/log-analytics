53bee0f90271691641897a379f8df7577e55ecc5
luqmansen <luqmansen@gmail.com>
2019-11-22 18:22:39 +0700

    add TestParseLine

--- cmd/analytics_test.go
+++ cmd/analytics_test.go
@@ -9,15 +9,27 @@ func TestIsOpenNewFile(t *testing.T) {
 	printCount = 10
 
 	b := isOpenNewFile()
-
 	if b == false {
 		t.Errorf("isOpenNewFile() failed, expected %v, got %v", true, b)
 	}
-	printCount++
 
+	printCount++
 	b = isOpenNewFile()
 	if b == true {
 		t.Errorf("isOpenNewFile() failed, expected %v, got %v", false, b)
 	}
+}
 
+func TestParseLine(t *testing.T){
+	s := []string{"a", "b", "c", "[stuff"}
+	str := parseLine(s)
+	if str != "stuff"{
+		t.Errorf("parseLine([]string{\"a\", \"b\", \"[stuff\", \"d\"}) failed, expected %v, got %v", "stuff", str)
+	}
+
+	s = []string{"a", "b", "c"}
+	str = parseLine(s)
+	if str != ""{
+		t.Errorf("parseLine([]string{\"a\", \"b\", \"c\"}) failed, expected %v, got %v", nil, str)
+	}
 }
